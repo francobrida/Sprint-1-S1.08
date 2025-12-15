e0045585dbae784423538a4943fcc283a7a54f36
francobrida <francobrida@gmail.com>
2025-12-15 12:55:42 +0100

    Nivel2 Ejercicio 1 corrected and tests ok!

---
+++ Nivel2/Ejercicio1/tests/NumberCheckerTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+require_once __DIR__ . '/../src/NumberChecker.php';
+
+class NumberCheckerTest extends TestCase {
+
+	public static function NumberCheckerProvider(): array {
+		return [
+			[3, false, true], 
+			[4, true, true], 
+			[-5, false, false], 
+			[-4, true, false]
+		];
+	}
+
+	#[DataProvider('NumberCheckerProvider')]
+	
+	public function testNumberChecker(int $num, bool $even, bool $positive) : void {
+		$numberChecker = new NumberChecker($num);
+
+		$this->assertSame($even, $numberChecker->isEven());
+		$this->assertSame($positive, $numberChecker->isPositive());
+		
+	}
+
+}
+
+?>
+
