19efac11dc6f11a8b681a3ad5485bf4b66f1bfd6
francobrida <francobrida@gmail.com>
2025-12-15 13:10:41 +0100

    Nivel2 Ejercicio 2 First Commit and tests ok

---
+++ Nivel2/Ejercicio2/tests/SpeedSensorTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+use function PHPUnit\Framework\assertEquals;
+
+require_once __DIR__ . '/../src/SpeedSensor.php';
+
+class SpeedSensorTest extends TestCase {
+
+    public static function speedDataProvider() : array {
+
+        return [
+            [27, "Very slow"],
+            [40, "Suitable speed"],
+            [61, "Slight excess"],
+            [90, "Moderate excess"],
+            [120,"Serious excess"]
+        ];
+    }
+
+    #[DataProvider('speedDataProvider')]
+
+    public function testSpeedSensor(int $num, string $speed) : void {
+        $speedSensor = new SpeedSensor($num);
+
+        assertEquals($speed, $speedSensor->rateSpeed());
+
+    }
+
+}
+
+?>
